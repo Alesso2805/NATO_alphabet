6054062f6ce5c8d67f6decaf1f70fcd5de19d378
alesso2805 <alessandrodp2222@gmail.com>
2025-03-24 22:45:24 -0500

    upgraded searching for words with KeyError except handling

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 student_dict = {
     "student": ["Angela", "James", "Lily"], 
     "score": [56, 76, 98]
@@ -8,8 +10,7 @@ for (key, value) in student_dict.items():
     #Access key and value
     pass
 
-import pandas
-student_data_frame = pandas.DataFrame(student_dict)
+student_data_frame = pd.DataFrame(student_dict)
 
 #Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
@@ -19,7 +20,6 @@ for (index, row) in student_data_frame.iterrows():
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
-import pandas as pd
 #TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 df = pd.read_csv("nato_phonetic_alphabet.csv")
@@ -27,6 +27,15 @@ nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 print(nato_dict)
-word = input("Enter a word: ").upper()
-output_list = [nato_dict[letter] for letter in word]
-print(output_list)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
